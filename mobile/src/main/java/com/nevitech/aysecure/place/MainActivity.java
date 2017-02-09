ff4c61d7c070b8cd4fb27181a7d309754fbd35fe
emre <k.emre29@hotmail.com>
2017-02-09 08:46:55 +0300

    second commit

--- mobile/src/main/java/com/nevitech/aysecure/place/MainActivity.java
+++ mobile/src/main/java/com/nevitech/aysecure/place/MainActivity.java
@@ -2,27 +2,24 @@ package com.nevitech.aysecure.place;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 
 
 import com.nevitech.aysecure.R;
-import com.nevitech.aysecure.place.gps.GPSTracker;
+import com.nevitech.aysecure.place.logger.NevitechLoggerActivity;
 
 
 public class MainActivity extends AppCompatActivity
 
 {
+    Button b1;
 
-    Button         btnGps;
-    TextView       latText;
-    TextView       lonText;
-
-    GPSTracker     gpsTracker;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -31,45 +28,14 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        latText = (TextView)findViewById(R.id.latText);
-        lonText = (TextView)findViewById(R.id.lonText);
-        btnGps  = (Button)findViewById(R.id.btnGPSBul);
-
-        btnGps.setOnClickListener(new View.OnClickListener()
-
-        {
-
+        b1=(Button)findViewById(R.id.btnGPSBul);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
-
             {
-
-                gpsTracker=new GPSTracker(MainActivity.this);
-
-                if(gpsTracker.canGetLocation())
-
-                {
-
-                    double latitude  = gpsTracker.getLatitude();
-                    double longitude = gpsTracker.getLongitude();
-                    String lat       = latitude+"";
-                    String lon       = longitude+"";
-
-                    latText.setText(lat);
-                    lonText.setText(lon);
-                    //Toast.makeText(getApplicationContext(),"enlem   :"+latitude+"boylam  :"+longitude,Toast.LENGTH_SHORT).show();
-
-                }
-                else
-
-                {
-                    gpsTracker.showSettingsAlert();
-
-                }
-
+                Intent i=new Intent(MainActivity.this, NevitechLoggerActivity.class);
+                startActivity(i);
             }
-
         });
 
     }
